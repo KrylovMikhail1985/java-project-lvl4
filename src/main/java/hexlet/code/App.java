0f77fa65a4afbce7a8cb86bb6444cd0c62aa1db7
KrylovMikhail1985 <89261789846@mail.ru>
2022-07-28 22:14:17 +0300

    second step in progress

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,12 +3,12 @@
  */
 package hexlet.code;
 
-public final class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import io.javalin.Javalin;
 
+public final class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("I Love You!!! \nAnd now I can do it!!!"));
     }
 }
+
