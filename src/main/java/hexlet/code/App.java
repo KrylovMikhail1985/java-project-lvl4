8e0401cbbb8284a8b80a85e98fe3b2830cfd43a6
krylov <89261789846@mail.ru>
2022-07-30 12:06:39 +0300

    try to deploy. Corredted App class

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,16 +3,18 @@ package hexlet.code;
 import io.javalin.Javalin;
 
 public final class App {
-    static Javalin app;
+//    static Javalin app;
     public static void main(String[] args) {
-        getApp();
+        Javalin app = getApp();
         app.start();
     }
-    public static void getApp() {
-        app = Javalin.create(config -> {
+    public static Javalin getApp() {
+
+        Javalin app = Javalin.create(config -> {
             config.enableDevLogging(); // enable extensive development logging for http and websocket
         });
         app.get("/", ctx -> ctx.result("I Love You!!! \nAnd now I can do it!!!"));
+        return app;
     }
 }
 
