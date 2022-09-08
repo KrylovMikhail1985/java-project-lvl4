9359d70fc14524a3f4b50fec26e4563fd479e288
mikhailkrylov <89261789846@mail.ru>
2022-09-08 23:53:26 +0300

    in progress..

--- src/main/java/hexlet/code/domain/Url.java
+++ src/main/java/hexlet/code/domain/Url.java
@@ -1,13 +1,14 @@
 package hexlet.code.domain;
 
 import io.ebean.Model;
+import io.ebean.annotation.DbName;
 import io.ebean.annotation.NotNull;
 import io.ebean.annotation.WhenCreated;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.Date;
-
+@DbName("development")
 @Entity
 // point to name of the table. Default name is the class's name
 //@Table(name = "urls")
