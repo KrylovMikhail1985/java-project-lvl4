f7e442d87ec1352e5ec5e3613e3d08afd173a854
mikhailkrylov <89261789846@mail.ru>
2022-09-08 19:58:34 +0300

    in progress..

--- src/main/java/hexlet/code/MigrationGenerator.java
+++ src/main/java/hexlet/code/MigrationGenerator.java
@@ -6,24 +6,11 @@ import java.io.IOException;
 
 public final class MigrationGenerator {
     public static void main(String[] args) throws IOException {
-//        DataSourceConfig dataSourceConfig = new DataSourceConfig();
-//        dataSourceConfig.setUsername("sa");
-//        dataSourceConfig.setPassword("");
-//        dataSourceConfig.setUrl("jdbc:h2:mem:myapp;");
-//
-//// configuration ...
-//        DatabaseConfig config = new DatabaseConfig();
-//        config.setDataSourceConfig(dataSourceConfig);
-//
-//// create database instance
-//        Database database = DatabaseFactory.create(config);
-
-
         // Создаём миграцию
         DbMigration dbMigration = DbMigration.create();
         // Указываем платформу, в нашем случае H2
-        dbMigration.addPlatform(Platform.H2, "h2");
-//        dbMigration.addPlatform(Platform.POSTGRES, "postgres");
+//        dbMigration.addPlatform(Platform.H2, "h2");
+        dbMigration.addPlatform(Platform.POSTGRES, "postgres");
         // Генерируем миграцию
         dbMigration.generateMigration();
     }
